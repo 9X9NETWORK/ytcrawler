85cf04079215d8fa5db5ab00871a286e8e894ca9
Louis Jeng <louisje@gmail.com>
2014-04-16 01:53:10 +0000

    config (local only)

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
-$apiserver = 'www.9x9.tv';
-$dbhost = 'ec2-54-245-100-224.us-west-2.compute.amazonaws.com';
+$apiserver = 'localhost:8080';
+$dbhost = 'localhost';
 $dbuser = 'root';
-$dbpass = 'letlet';
+$dbpass = '';
 
 ?>
