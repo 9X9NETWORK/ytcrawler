a6823174aee7e9bd7146651f299d7356ef0c44d0
Ubuntu <ubuntu@channelwatch.9x9.tv>
2014-04-30 23:42:39 +0000

    dayparting cache

---
+++ dayparting_cache.py
@@ -0,0 +1,18 @@
+# it's to clean the dayparting channel. not very related to ytcrawler project. deployed in the same machine though.
+import urllib, urllib2
+import time
+
+host = "api.flipr.tv"
+
+url = "http://" + host + "/wd/daypartCache?mso=9x9&lang=en"
+print "url:" + url
+stream = urllib.urlopen(url)
+stream.close()
+
+for i in range(0, 24):
+   url = "http://" + host + "/playerAPI/portal?type=whatson&v=40&time=" + str(i)
+   print url
+   stream = urllib.urlopen(url)
+   stream.close()
+   time.sleep(10)
+
